7fb36668b278d67aaf6b7ca38f070d4c061496f7
dsushanta <dasgupta.sushanta@gmail.com>
2019-09-25 00:54:26 +0530

    added code to download all songs in a playlist

--- com/bravo/johny/songdownloader/YouTubeDownloaderFromPlaylistSearch.py
+++ com/bravo/johny/songdownloader/YouTubeDownloaderFromPlaylistSearch.py
@@ -40,20 +40,28 @@ def getYouTubePlaylists():
 def getAllSongsFromAllPlayLists():
     youtube = build('youtube', 'v3', developerKey=GOOGLE_API_KEY)
     for playListId in playListIds:
-        req = youtube.playlistItems().list(playlistId=playListId, part='snippet', maxResults=300)
-        result = req.execute()
-        r_json_str = json.dumps(result)
-        r_json = json.loads(r_json_str)
-        items = r_json.get('items')
+        resultCount = 0
         songTitles = []
         videoIds = []
-        for item in items:
-            title = item.get('snippet').get('title')
-            if title == 'Private video':
-                continue
-            videoId = item.get('snippet').get('resourceId').get("videoId")
-            songTitles.append(title)
-            videoIds.append(videoId)
+        req = youtube.playlistItems().list(playlistId=playListId, part='snippet', maxResults=50)
+        while True:
+            result = req.execute()
+            r_json_str = json.dumps(result)
+            r_json = json.loads(r_json_str)
+            items = r_json.get('items')
+            nextPageToken = r_json.get('nextPageToken')
+            for item in items:
+                title = item.get('snippet').get('title')
+                if title == 'Private video':
+                    continue
+                videoId = item.get('snippet').get('resourceId').get("videoId")
+                songTitles.append(title)
+                videoIds.append(videoId)
+
+            resultCount = resultCount + 50
+            if nextPageToken is None or resultCount > MAXIMUM_NUMBER_OF_SONGS_TO_DOWNLOAD:
+                break
+            req = youtube.playlistItems().list(playlistId=playListId, part='snippet', maxResults=50, pageToken=nextPageToken)
 
         songTitlesOfAllPlaylists.append(songTitles)
         videoIdsOfAllPlaylists.append(videoIds)
@@ -65,6 +73,7 @@ def displayPlaylistWithItsSongs():
     while i < NO_OF_PLAYLIST_TO_DISPLAY:
         print("Playlist number : " + str(i + 1) + "\n")
         print("Playlist title : " + playListTitles[i] + "\n")
+        print("Number of songs in the playlist : " + str(len(songTitlesOfAllPlaylists[i])))
         print("Songs in the playlist : " + str(songTitlesOfAllPlaylists[i]) + "\n")
         print("---------------------------------------------------------------------------------------\n")
         i = i+1
@@ -94,6 +103,7 @@ DOWNLOAD_LOCATION = '/tmp'
 YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v="
 GOOGLE_API_KEY = "AIzaSyApLwmFqEBOS_bxj1DJRv_ULwWO-PKWQLY"
 NO_OF_PLAYLIST_TO_DISPLAY = 6
+MAXIMUM_NUMBER_OF_SONGS_TO_DOWNLOAD = 400
 
 song_youtube_url_list = []
 playListIds = []
